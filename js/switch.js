089c2f82316466e6f85432edf3e7cc291e4f802f
Michael Victor <radmakson@gmail.com>
2023-08-18 02:13:07 +0100

    stored: pref

--- js/switch.js
+++ js/switch.js
@@ -1,6 +1,21 @@
 const switchButton = document.getElementById("switch-toggle");
 const body = document.body;
 
+// Check if the user's preference is stored in localStorage
+const checkDarkMode = localStorage.getItem("darkMode");
+
+// If dark mode was previously selected, apply it
+if (checkDarkMode === "true") {
+  body.classList.add('dark-mode');
+}
+
 switchButton.addEventListener("click", () => {
-  body.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+
+    // Update the preference in localStorage based on the current state
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem("darkMode", "true");
+    } else {
+        localStorage.setItem("darkMode", "false");
+    }
 });
